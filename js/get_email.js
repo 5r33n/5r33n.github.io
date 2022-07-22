87714b461f4657125a2cdf157148f15b14ef33a1
3greens <treecode@protonmail.com>
2022-07-22 14:57:30 -0400

    added js secure email

---
+++ js/get_email.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('#set-email').html('<a href="#">[show email]</a>');
+	$('#set-email').click( function () {
+		$('#set-email').html('<a href="'+'mai'+'lto'+':'+
+			'5r33ns'+'@g'+'mai'+'l.c'+'om">'+'ema'+
+			'il('+'5r33n'+'s@gm'+'ail.c'+'om)'+'</a>');
+	});
+});
